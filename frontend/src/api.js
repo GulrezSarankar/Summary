8771f860e3e1bbeefd92226337bd9aad7c08d26b
gulrez121 <gulrezsarankar16@gmail.com>
2025-12-12 10:56:10 +0530

    changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 const api = axios.create({
-  baseURL: "summary-production-303a.up.railway.app",
-  
+  baseURL: API_URL
 });
 
 api.interceptors.request.use((config) => {
